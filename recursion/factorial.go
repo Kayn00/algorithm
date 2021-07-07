b22c26c242f360dbb4e95bed449e6721d725e34b
hewenyan <wxhewenyan@gfpartner.com.cn>
2021-07-07 09:08:02 +0800

    dev

--- recursion/factorial.go
+++ recursion/factorial.go
@@ -1,1 +1,27 @@
-package recursion
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	start := time.Now().UnixNano()
+	fmt.Println(rescuvie(5))
+	//rescuvieTail(5, 1)
+	fmt.Println(time.Now().UnixNano() - start)
+}
+
+func rescuvie(n int) int {
+	if n == 1 {
+		return 1
+	}
+	return n * rescuvie(n-1)
+}
+
+func rescuvieTail(n int, a int) int {
+	if n == 1 {
+		return a
+	}
+	return rescuvieTail(n-1, n*a)
+}
--- recursion/fibonacci.go
+++ recursion/fibonacci.go
@@ -1,1 +1,21 @@
 package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(fibonacci(5))
+}
+
+func fibonacci(n int) int {
+	if n < 2 {
+		return 1
+	}
+	return fibonacci(n-1) + fibonacci(n-2)
+}
+
+func fibonacci1(n, a1, a2 int) int {
+	if n == 0 {
+		return a1
+	}
+	return fibonacci1(n-1, a2, a1+a2)
+}
